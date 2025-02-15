85b9ad62a82d931062f9fa9b154e744785625533
ThamizhAnban <thamizhanban2006@gmail.com>
2025-02-15 10:01:48 +0530

    Database connected

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.listen(PORT,()=>{
 const uri = process.env.uri;
 mongoose.connect(uri)
 .then(()=>{
-    console.log('Database connected successfully!')
+    console.log('Database has been connected successfully!')
 })
 .catch((err)=>{
     console.log('Error',err);
