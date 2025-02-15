5c9bb9cc6f1510a50be9dfe01e69c3a1e3db86e0
ThamizhAnban <thamizhanban2006@gmail.com>
2025-02-15 09:48:50 +0530

    database connected

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express();
 const PORT = 3000;
+const mongoose = require('mongoose');
+
 
 app.get("/ping",(req,res)=>{
     res.send("pong")
@@ -8,4 +10,13 @@ app.get("/ping",(req,res)=>{
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
+})
+
+const uri = process.env.uri;
+mongoose.connect(uri)
+.then(()=>{
+    console.log('Database connected successfully!')
+})
+.catch((err)=>{
+    console.log('Error',err);
 })
