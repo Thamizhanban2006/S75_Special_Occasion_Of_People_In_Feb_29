1e63ba8ea6c845a3c717eca7f2f4c0c38698681d
ThamizhAnban <thamizhanban2006@gmail.com>
2025-02-20 17:33:33 +0530

    CRUD operations completed!

--- server.js
+++ server.js
@@ -1,28 +1,24 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const itemsRoutes = require("./routes"); // Import routes
+
+dotenv.config();
 const app = express();
-
 const PORT = 3000;
-const mongoose = require('mongoose');
 
+// Middleware
+app.use(express.json()); // Needed to parse JSON requests
+app.use("/api", itemsRoutes); // Prefix routes with `/api`
 
-require('dotenv').config()
-
-
-
-
-app.get("/ping",(req,res)=>{
-    res.send("pong")
-});
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
-
+// Database connection
 const uri = process.env.uri;
-mongoose.connect(uri)
-.then(()=>{
-    console.log('Database has been connected successfully!')
-})
-.catch((err)=>{
-    console.log('Error',err);
-})
+mongoose
+  .connect(uri)
+  .then(() => console.log("Database has been connected successfully!"))
+  .catch((err) => console.log("Error", err));
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
