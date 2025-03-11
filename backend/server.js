ea14d791fb7d0c6fa32b76a11aa86011923b5764
ThamizhAnban <thamizhanban2006@gmail.com>
2025-03-11 17:45:14 +0530

    Completed

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const app = express();
 const PORT = 3000;
 const uri = process.env.uri;
 
+let users = [{id: 1, name: "Thamizh"}]
+
 
 app.use(cors());
 app.use(express.json()); 
@@ -17,22 +19,40 @@ mongoose.connect(uri)
   .catch((err) => console.log("Error:", err));
 
   
-app.get("/ping", (req, res) => {
-  res.send("pong");
-});
-
+  // const express = require("express");
+  // const router = express.Router();
+  
+  
+  // let items = [{ id: 1, name: "Shankar" }];
+  
+  
+app.post("/items", (req, res) => {
+    const { name } = req.body;
+    if (!name) return res.status(400).json({ message: "Name is required" });
+  
+    const newItem = { id: users.length + 1, name };
+    users.push(newItem);
+    res.status(201).json(newItem);
+  });
+  
+  app.get("/items", (req, res) => {
+    res.json(users);
+  });
+  
+  
+  // module.exports = router;
 // Define an API Route to Fetch Data
-const EntitySchema = new mongoose.Schema({ name: String });
-const Entity = mongoose.model("Entity", EntitySchema);
+// const EntitySchema = new mongoose.Schema({ name: String });
+// const Entity = mongoose.model("Entity", EntitySchema);
 
-app.get("/entities", async (req, res) => {
-  try {
-    const entities = await Entity.find();
-    res.json(entities);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+// app.get("/entities", async (req, res) => {
+//   try {
+//     const entities = await Entity.find();
+//     res.json(entities);
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
 
 // Start Server
 app.listen(PORT, () => {
--- frontend/src/NewComponent.jsx
+++ frontend/src/NewComponent.jsx
@@ -3,9 +3,24 @@ import './App.css'
 
 function NewComponent() {
     const [Count,setCount]=useState(0);
+    const [Ent, setEnt] = useState([])
 
     const handleIncrement=()=>{
         setCount((Count)=>Count+1);
+    }
+
+    const fetchData = async()=>{
+      try{
+        const response = await fetch("http://localhost:3000/items");
+        if(!response.ok){
+          console.log("error")
+        }
+        const data = await response.json();
+        setEnt(data);
+      }
+      catch(err){
+        console.log("Error",err)
+      }
     }
 
 
@@ -14,8 +29,19 @@ function NewComponent() {
         <button className='like-button' onClick={handleIncrement}>
         Like:{Count}
         </button>
+        <button onClick={fetchData}>Fetch Data</button>
+        <p>{
+            Ent.map((item,i)=>(
+              <div key={i}>
+                <h2>{item.id}</h2>
+                <p>{item.name}</p>
+              </div>
+
+            ))
+          }</p>
     </div>
   )
 }
 
 export default NewComponent;
+
